16b777e897db2c66414b03dff3c0e14d7f617f76
Marit de Vries <Maritdevries1@live.nl>
2022-12-14 16:16:09 +0100

    Changed form Submit to XMLHttp request and added success/fail message on submit

--- main.js
+++ main.js
@@ -19,3 +19,32 @@ navButtons.forEach((button) => {
 })
 
 toggleButton.addEventListener("click", toggleMobileMenu);
+
+
+function submitContactForm() {
+    const visitorName = document.getElementById("visitorName").value;
+    const visitorEmail = document.getElementById("visitorEmail").value;
+    const visitorMessage = document.getElementById("visitorMessage").value;
+
+    const submitMessage = document.getElementById("form-submit__message");
+
+    if (visitorName && visitorEmail && visitorMessage) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function() {
+            submitMessage.innerHTML = "Message sent successfully!";
+        }
+
+    xhttp.open("POST", "contact-form-submit.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(`name=${visitorName}&email=${visitorEmail}&message=${visitorMessage}`);
+
+    document.getElementById("visitorName").value = "";
+    document.getElementById("visitorEmail").value = "";
+    document.getElementById("visitorMessage").value = "";
+    } else {
+        submitMessage.innerHTML = "Please make sure to fill in all the required fields!";
+    }
+}
+
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", submitContactForm);
